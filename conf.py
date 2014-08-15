b18087207474ac6d7e194897bbb6db6d86628a31
Christoph Meier <meier.c@sfeir.lu>
2014-08-15 16:53:16 +0200

    Fix conflict with rtfd theme

--- conf.py
+++ conf.py
@@ -11,7 +11,12 @@
 
 import sys
 import os
-import sphinx_rtd_theme
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -98,8 +103,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'default'
+html_theme = 'default'
+if not on_rtd:
+  html_theme = 'sphinx_rtd_theme'  
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -107,8 +113,9 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#html_theme_path = []
+html_theme_path = []
+if not on_rtd:
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
